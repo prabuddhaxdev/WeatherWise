2528ae0f7672ed2acfecc063ee7611c63179234d
prabuddha610 <prabuddha610@gmail.com>
2025-08-01 19:29:54 +0530

    Add Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom"
 import { Layout } from "./components/layout"
+import { ThemeProvider } from "./context/theme-provider"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Layout>Hello</Layout>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>Hello</Layout>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
