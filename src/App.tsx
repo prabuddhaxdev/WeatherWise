8dbc7cc119059ee8afe201653e3c83c23639fa70
prabuddha610 <prabuddha610@gmail.com>
2025-08-02 01:10:20 +0530

    Created Queries for Weather API

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import WeatherDashboard from "./pages/weather-dashboard"
 import CityPage from "./pages/city-page"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,4 +1,8 @@
-
+import {
+  useForecastQuery,
+  useReverseGeocodeQuery,
+  useWeatherQuery,
+} from "@/hooks/use-weather";
 import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 import { Button } from "../components/ui/button";
 import { MapPin, AlertTriangle, RefreshCw } from "lucide-react";
@@ -13,14 +17,17 @@ export default function WeatherDashboard() {
     getLocation,
   } = useGeolocation();
 
-  console.log(coordinates);
-
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+  const locationQuery = useReverseGeocodeQuery(coordinates);
 
   // Function to refresh all data
   const handleRefresh = () => {
     getLocation();
     if (coordinates) {
-      // reload weather data
+      weatherQuery.refetch();
+      forecastQuery.refetch();
+      locationQuery.refetch();
     }
   };
 
@@ -60,24 +67,27 @@ export default function WeatherDashboard() {
     );
   }
 
+  const locationName = locationQuery.data?.[0];
 
-  // if (weatherQuery.error || forecastQuery.error) {
-  //   return (
-  //     <Alert variant="destructive">
-  //       <AlertTriangle className="h-4 w-4" />
-  //       <AlertTitle>Error</AlertTitle>
-  //       <AlertDescription className="flex flex-col gap-4">
-  //         <p>Failed to fetch weather data. Please try again.</p>
-  //         <Button variant="outline" onClick={handleRefresh} className="w-fit">
-  //           <RefreshCw className="mr-2 h-4 w-4" />
-  //           Retry
-  //         </Button>
-  //       </AlertDescription>
-  //     </Alert>
-  //   );
-  // }
+  if (weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed to fetch weather data. Please try again.</p>
+          <Button variant="outline" onClick={handleRefresh} className="w-fit">
+            <RefreshCw className="mr-2 h-4 w-4" />
+            Retry
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
 
-
+  if (!weatherQuery.data || !forecastQuery.data) {
+    return <WeatherSkeleton />;
+  }
 
   return (
     <div className="space-y-4">
@@ -88,30 +98,30 @@ export default function WeatherDashboard() {
           variant="outline"
           size="icon"
           onClick={handleRefresh}
-          // disabled={weatherQuery.isFetching || forecastQuery.isFetching}
+          disabled={weatherQuery.isFetching || forecastQuery.isFetching}
         >
           <RefreshCw
-            // className={`h-4 w-4 ${
-            //   weatherQuery.isFetching ? "animate-spin" : ""
-            // }`}
+            className={`h-4 w-4 ${
+              weatherQuery.isFetching ? "animate-spin" : ""
+            }`}
           />
         </Button>
       </div>
 
-      <div className="grid gap-6">
+      {/* <div className="grid gap-6">
         <div className="flex flex-col lg:flex-row gap-4">
-          {/* <CurrentWeather
+          <CurrentWeather
             data={weatherQuery.data}
             locationName={locationName}
           />
-          <HourlyTemperature data={forecastQuery.data} /> */}
+          <HourlyTemperature data={forecastQuery.data} />
         </div>
 
         <div className="grid gap-6 md:grid-cols-2 items-start">
-          {/* <WeatherDetails data={weatherQuery.data} />
-          <WeatherForecast data={forecastQuery.data} /> */}
+          <WeatherDetails data={weatherQuery.data} />
+          <WeatherForecast data={forecastQuery.data} />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
