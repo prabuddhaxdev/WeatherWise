dd4d3c7603cf308265f9e4a5272b38e1a47366c6
prabuddha610 <prabuddha610@gmail.com>
2025-08-02 01:23:02 +0530

    Completed Current Weather Component

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -8,6 +8,7 @@ import { Button } from "../components/ui/button";
 import { MapPin, AlertTriangle, RefreshCw } from "lucide-react";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import WeatherSkeleton from "../components/loading-skeleton";
+import { CurrentWeather } from "@/components/current-weather";
 
 export default function WeatherDashboard() {
   const {
@@ -108,20 +109,20 @@ export default function WeatherDashboard() {
         </Button>
       </div>
 
-      {/* <div className="grid gap-6">
+      <div className="grid gap-6">
         <div className="flex flex-col lg:flex-row gap-4">
           <CurrentWeather
             data={weatherQuery.data}
             locationName={locationName}
           />
-          <HourlyTemperature data={forecastQuery.data} />
+          {/* <HourlyTemperature data={forecastQuery.data} /> */}
         </div>
 
-        <div className="grid gap-6 md:grid-cols-2 items-start">
+        {/* <div className="grid gap-6 md:grid-cols-2 items-start">
           <WeatherDetails data={weatherQuery.data} />
           <WeatherForecast data={forecastQuery.data} />
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </div>
   );
 }
