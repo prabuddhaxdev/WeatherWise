1388678643d356285f030f6f71476f12c22ce311
prabuddha610 <prabuddha610@gmail.com>
2025-08-01 19:11:50 +0530

    Add initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
